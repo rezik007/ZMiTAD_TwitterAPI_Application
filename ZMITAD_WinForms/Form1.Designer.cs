d020c30fe2d2e1fd8ff005a22e03701c086771dd
Fabian3223 <fabiansporek@wp.pl>
2016-04-28 15:55:51 +0200

    mozliwosc zmiany przedzialu czasu na wykresie, nowe wykresy, troche statystyki w labelu (ilosci srednie slow, znakow)

--- ZMITAD_WinForms/Form1.Designer.cs
+++ ZMITAD_WinForms/Form1.Designer.cs
@@ -28,44 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(311, 13);
+            this.textBox1.Location = new System.Drawing.Point(311, 37);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(263, 300);
+            this.textBox1.Size = new System.Drawing.Size(263, 304);
             this.textBox1.TabIndex = 0;
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(3, 13);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(3, 37);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(300, 300);
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(300, 276);
             this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(177, 317);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 320);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Co ile czasu grupuj na wykresie:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 352);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "LABEL STATUS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(311, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Historia zdarzen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Typ wykresu:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(80, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(218, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(586, 353);
+            this.ClientSize = new System.Drawing.Size(586, 377);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -81,6 +147,12 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
--- ZMITAD_WinForms/Form1.cs
+++ ZMITAD_WinForms/Form1.cs
@@ -19,10 +19,143 @@ namespace ZMITAD_WinForms
             InitializeComponent();
 
             start = DateTime.Now.Ticks / (decimal)TimeSpan.TicksPerMillisecond; ;
+
+            comboBox2.Items.Add("Ilosc statusow na przedzial czasu");
+            comboBox2.Items.Add("Ilosc znakow na przedzial czasu");
+            comboBox2.Items.Add("Ilosc slow na przedzial czasu");
+            comboBox2.Items.Add("Histogram (dlugosc statusu w znakach)");
+            comboBox2.SelectedIndex = 0;
         }
 
         private int ilosc = 0;
         private int popMin = -1;
+        private decimal teraz;
+        private List<decimal> czasyNadejscia = new List<decimal>();
+        private List<int> ilosciSlow = new List<int>();
+        private List<int> ilosciZnakow = new List<int>();
+        private int sumaDlugosciStatusow;
+        private int sumaIlosciSlow;
+        // dlugosc najkrotszego i nalduzszego statussu
+        private int ileZnakowMin, ileZnakowMax;
+
+        private double getCzasPrzedzialu()
+        {
+            /*
+                     comboBox1.Items.Add("Co 1 sekunda");
+            comboBox1.Items.Add("Co 30 sekund");
+            comboBox1.Items.Add("Co minuta");
+            comboBox1.Items.Add("Co 5 minut");
+            */
+
+            int ileSekund = 1;
+            if (comboBox1.SelectedIndex == 0)
+                ileSekund = 1;
+            else if (comboBox1.SelectedIndex == 1)
+                ileSekund = 30;
+            else if (comboBox1.SelectedIndex == 2)
+                ileSekund = 60;
+            else if (comboBox1.SelectedIndex == 3)
+                ileSekund = 60 * 5;
+            return ileSekund * 1000.0;
+        }
+        private void odswiezWykres()
+        {
+            chart1.Series[0].Points.Clear();
+            if (comboBox2.SelectedIndex == 3)
+            {
+                comboBox1.Enabled = false;
+                chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+
+                // histogram
+                double przedzial = ileZnakowMax - ileZnakowMin;
+                int ileSlupkow = 10; // ilosc slupkow
+                double skok = przedzial / ileSlupkow;
+                double min = ileZnakowMin;
+                for(int i = 0; i < ileSlupkow; i++)
+                {
+                    // slupek i-ty
+                    double max = min + skok;
+                    int ileStatusow = 0;
+                    // zlicz ile mamy takich statusow
+                    for(int j = 0; j < ilosciZnakow.Count; j++)
+                    {
+                        int znakow = ilosciZnakow[j];
+                        if(znakow >= min && znakow < max)
+                        {
+                            ileStatusow++;
+                        }
+                    }
+                    chart1.Series[0].Points.AddXY(i, ileStatusow);
+                    min += skok;
+                }
+            }
+            else
+            {
+                comboBox1.Enabled = true;
+                chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                chart1.Series[0].Points.AddXY(0, 0);
+                int px = -1;
+                int ile = 0;
+                for (int i = 0; i < czasyNadejscia.Count; i++)
+                {
+                    // czas w milisekundach
+                    double x = ((double)czasyNadejscia[i]) / getCzasPrzedzialu();
+                    int ix = (int)x;
+                    if (px != ix)
+                    {
+                        if (px != -1)
+                        {
+                            chart1.Series[0].Points.AddXY(ix, ile);
+                        }
+                        px = ix;
+                        ile = 0;
+                    }
+
+
+                    // dodaj do ile, to co zliczamy
+                    if (comboBox2.SelectedIndex == 0)
+                    {
+                        // zliczamy statusy - dodaj 1
+                        ile++;
+                    }
+                    else if (comboBox2.SelectedIndex == 1)
+                    {
+                        // zliczamy znaki - dodaj ilosc znakow i-tego statusu
+                        ile += ilosciZnakow[i];
+                    }
+                    else if (comboBox2.SelectedIndex == 2)
+                    {
+                        // zliczamy slowa - dodaj ilosc slow i-tego statusu
+                        ile += ilosciSlow[i];
+                    }
+                }
+                if (ile != 0 && px != -1)
+                {
+                    chart1.Series[0].Points.AddXY(px, ile);
+                }
+            }
+        }
+        public double getSredniaDlStatusu()
+        {
+            double sr = sumaDlugosciStatusow / (double)czasyNadejscia.Count;
+            return sr;
+        }
+        public double getSredniaIloscSlowStatusu()
+        {
+            double sr = sumaIlosciSlow / (double)czasyNadejscia.Count;
+            return sr;
+        }
+        public string getCzasDzialaniaStr()
+        {
+            int sek = (int)(((double)teraz) / 1000);
+            if(sek > 60)
+            {
+                int minut = sek / 60;
+                sek %= 60;
+                return minut.ToString() + " minut i " + sek.ToString() + " sekund";
+            }
+            return sek.ToString() + " sekund";
+        }
         public void add(status s)
         {
             // czy wywolano z innego watku niz gui?
@@ -41,45 +174,91 @@ namespace ZMITAD_WinForms
                 return;
             }
             // to sie juz wykona w watku gui
-            textBox1.Text += "Tresc statusu " + s.text + Environment.NewLine;
-
-
-            decimal teraz = DateTime.Now.Ticks / (decimal)TimeSpan.TicksPerMillisecond; ;
-            teraz = teraz - start;
-            double minuta = ((double)teraz / 1000.0) / 10.0;
-            int imin = (int)minuta;
-            if(popMin == -1)
+            if(ilosciSlow.Count == 0)
             {
-                //test
-                popMin = imin;
-                ilosc++;
+                ileZnakowMin = ileZnakowMax = s.text.Length;
             }
             else
             {
-                if(popMin != imin)
-                {
-                    chart1.Series[0].Points.AddXY(popMin, ilosc);
-                    popMin = imin;
-                    ilosc = 0;
-                }
-                else
-                {
-
-                }
-                ilosc++;
+                if (ileZnakowMax < s.text.Length)
+                    ileZnakowMax = s.text.Length;
+                if (ileZnakowMin > s.text.Length)
+                    ileZnakowMin = s.text.Length;
             }
+            sumaDlugosciStatusow += s.text.Length;
+            int ileSlow = policzSlowa(s.text);
+            sumaIlosciSlow += ileSlow;
+            ilosciSlow.Add(ileSlow);
+            ilosciZnakow.Add(s.text.Length);
+            teraz = DateTime.Now.Ticks / (decimal)TimeSpan.TicksPerMillisecond; ;
+            teraz = teraz - start;
+            czasyNadejscia.Add(teraz);
+            odswiezWykres();
+            int sekunda = (int)((double)teraz / 1000.0);
+            textBox1.Text += "[sekunda "+sekunda+"]: "+ "Tresc statusu " + s.text + Environment.NewLine;
+            //double minuta = ((double)teraz / getCzasPrzedzialu());
+            //int imin = (int)minuta;
+            //if(popMin == -1)
+            //{
+            //    //test
+            //    popMin = imin;
+            //    ilosc++;
+            //}
+            //else
+            //{
+            //    if(popMin != imin)
+            //    {
+            //        chart1.Series[0].Points.AddXY(popMin, ilosc);
+            //        popMin = imin;
+            //        ilosc = 0;
+            //    }
+            //    else
+            //    {
+
+            //    }
+            //    ilosc++;
+            //}
+            label3.Text = "Przeanalizowano " + czasyNadejscia.Count + " statusow"
+                +
+                ". Program dziala " + getCzasDzialaniaStr()
+                +
+                " Sr. dl. statusu " + ((int)getSredniaDlStatusu()).ToString()
+                +
+                " Sr. ilosc slow statusu " + ((int)getSredniaIloscSlowStatusu()).ToString();
+
+                
         }
         public void CallToChildThread()
         {
             TwitterStream stream = new TwitterStream();
             stream.Stream2Queue(this);
         }
-        
+        private int policzSlowa(String text)
+        {
+            // mozna by ulepszyc
+            return text.Split(' ').Length;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             ThreadStart childref = new ThreadStart(CallToChildThread);
             Thread childThread = new Thread(childref);
             childThread.Start();
+
+            comboBox1.Items.Add("Co 1 sekunda");
+            comboBox1.Items.Add("Co 30 sekund");
+            comboBox1.Items.Add("Co minuta");
+            comboBox1.Items.Add("Co 5 minut");
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            odswiezWykres();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            odswiezWykres();
         }
     }
 }
