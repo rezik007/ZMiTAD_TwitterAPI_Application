0faf75ddbcc2fd57814e5b8d0d4c1d33129784c2
Fabian3223 <fabiansporek@wp.pl>
2016-04-28 16:28:49 +0200

    dodalem osobna Forme w ktorej mozna wyswietlic historie otrzymanych statusow w ListView (niedokonczone)

---
+++ ZMITAD_WinForms/Historia.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ZMITAD_WinForms
+{
+    partial class Historia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.Location = new System.Drawing.Point(13, 43);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(507, 311);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Kiedy";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Znakow";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Slow";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Tresc";
+            this.columnHeader5.Width = 260;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Odswiez";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Historia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 366);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Historia";
+            this.Text = "Historia";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Historia_FormClosed);
+            this.Load += new System.EventHandler(this.Historia_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ZMITAD_WinForms/Historia.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZMITAD_WinForms
+{
+    public partial class Historia : Form
+    {
+        private Form1 f;
+
+        public Historia(Form1 f1)
+        {
+            this.f = f1;
+            InitializeComponent();
+        }
+        private void odswiezListe()
+        {
+            listView1.Items.Clear();
+
+            for (int i = 0; i < f.getIleStatusow(); i++)
+            {
+                double d = (double)f.getCzasStatusu(i);
+                int czasSekund = (int)(d / 1000.0);
+                ListViewItem it = new ListViewItem(i.ToString());
+                it.SubItems.Add(czasSekund.ToString());
+                it.SubItems.Add(f.getIloscZnakow(i).ToString());
+                it.SubItems.Add(f.getIloscSlow(i).ToString());
+                listView1.Items.Add(it);
+            }
+        }
+        private void Historia_Load(object sender, EventArgs e)
+        {
+            odswiezListe();
+        }
+
+        private void Historia_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            f.zamknietoHistorie();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            odswiezListe();
+        }
+    }
+}
